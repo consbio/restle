224bb39ebf9f3769e2909c6a0e82dc6dcb67f117
nikmolnar <nik.molnar@consbio.org>
2014-12-31 18:33:15 -0800

    Added license and setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='restle',
+    description='A REST client framework',
+    keywords='rest,mapper,client',
+    version='0.0.1',
+    packages=['restle'],
+    requires=['six'],
+    url='https://github.com/consbio/restle',
+    license='BSD'
+)
