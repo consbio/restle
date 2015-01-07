eb0203bceb3bf59cc8cbe552ee87868ac928698e
nikmolnar <nik.molnar@consbio.org>
2015-01-07 13:13:58 -0800

    Added test command to setup.py, added travis config

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
+from distutils.cmd import Command
 from distutils.core import setup
+import subprocess
+import sys
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
 
 setup(
     name='restle',
@@ -6,7 +23,9 @@ setup(
     keywords='rest,mapper,client',
     version='0.0.1',
     packages=['restle'],
-    requires=['six'],
+    requires=['six', 'requests'],
     url='https://github.com/consbio/restle',
-    license='BSD'
+    license='BSD',
+    tests_require=['pytest', 'httpretty', 'mock'],
+    cmdclass={'test': PyTest}
 )
